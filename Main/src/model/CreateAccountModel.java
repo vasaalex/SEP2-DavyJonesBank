8e5dd7c132987e18d9ae2281da8a29525850bcde
Mvycas <martynas.vycas@hotmail.com>
2021-11-20 20:26:09 +0100

    Added two classes: CreateAccountModelManager and CreateAccountModel

---
+++ Main/src/model/CreateAccountModel.java
@@ -0,0 +1,12 @@
+package model;
+
+public interface CreateAccountModel
+{
+  public void sendUsername(String username);
+  public void sendPassword(String password);
+  public void sendFName(String fName);
+  public void sendIName(String lName);
+  public void sendEmail(String email);
+  public void sendPhone(long phone);
+
+}
---
+++ Main/src/model/CreateAccountModelManager.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeSupport;
+
+public class CreateAccountModelManager implements CreateAccountModel
+{
+
+  private PropertyChangeSupport support = new PropertyChangeSupport(this);
+  private Client client;
+
+  public CreateAccountModelManager()
+  {
+    client.startClient();
+    client.addListener("Account Creation State", this::onNewAccountCreated);
+  }
+
+  public void onNewAccountCreated(PropertyChangeEvent propertyChangeEvent){
+    support.fireIndexedPropertyChange(propertyChangeEvent);
+  }
+
+  @Override public void sendUsername(String username)
+  {
+    return client.sendUsername(username);
+  }
+
+  @Override public void sendPassword(String password)
+  {
+    return client.sendPassword(password);
+  }
+
+  @Override public void sendFName(String fName)
+  {
+    return client.sendFname(fName);
+  }
+
+  @Override public void sendIName(String lName)
+  {
+    return client.sendLName(lName);
+  }
+
+  @Override public void sendEmail(String email)
+  {
+    return client.sendEmail(email);
+  }
+
+  @Override public void sendPhone(long phone)
+  {
+    return client.sendPhone(phone);
+  }
+}
+
